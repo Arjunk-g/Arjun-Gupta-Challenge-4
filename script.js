45238e8cb7bcbd7efec56f56c933dd443b11b208
arjun gupta <arjunkarangupta100@gmail.com>
2022-09-23 02:46:13 -0400

    Began making classes for cards including the beginning display card, and linked the first starting button to the JS file

---
+++ script.js
@@ -0,0 +1,14 @@
+var questionCards = ['0','1','2','3','4','5']
+
+var startButton = document.querySelector("#begin-Quiz");
+
+// so far I can console log a button which is already a big step forward
+
+function nextCardTest() {
+    console.log('bruh');
+}
+
+startButton.addEventListener("click", function() {
+nextCardTest();
+})
+
