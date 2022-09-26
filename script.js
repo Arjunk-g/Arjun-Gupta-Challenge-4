5a9bf8f761b9c47b793636c54eb5c07f86c41b05
arjun gupta <arjunkarangupta100@gmail.com>
2022-09-26 04:18:05 -0400

    Added timer still working on how to get it to stop at the end

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-var questionCards = ['0','1','2','3','4','5']
+var differentCards = ['0','1','2','3','4','5']
 
 var startButton = document.querySelector("#begin-Quiz");
 
+var timerEl = document.querySelector(".timer")
+
+var secondsLeft = 60;
+
+
 // so far I can console log a button which is already a big step forward
 
-function nextCardTest() {
-    console.log('bruh');
+startButton.setAttribute
+
+function setTimer(){
+
+   var timerInterval = setInterval(function() {
+   secondsLeft--;
+   timerEl.textContent = "Until Game Over:" + secondsLeft;
+
+   
+      if (secondsLeft === 0) 
+      {
+         clearInterval(timerInterval);
+         sendEndMessage();
+      }
+
+
+}, 1000);
+
 }
 
+//going to use this function to display end of timer screen or I guess the end screen
+
+function sendEndMessage(){
+   timerEl.textContent = " ";
+   console.log("bruh");
+
+}
+
+
+
+// putting buttons down here
+
+
 startButton.addEventListener("click", function() {
-nextCardTest();
+setTimer();
 })
 
